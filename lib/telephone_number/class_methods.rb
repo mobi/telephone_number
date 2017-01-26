f60b26e5c9aaff3e7e5de36c292e13b923142e31
Josh Wetzel <joshwetzel@users.noreply.github.com>
2017-01-25 19:49:52 -0500

    Add invalid class method (#3)

--- lib/telephone_number/class_methods.rb
+++ lib/telephone_number/class_methods.rb
@@ -7,5 +7,9 @@ module TelephoneNumber
     def valid?(number, country, keys = [])
       parse(number, country).valid?(keys)
     end
+
+    def invalid?(*args)
+      !valid?(*args)
+    end
   end
 end
