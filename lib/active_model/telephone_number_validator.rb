579acdfd5f9aadda31e2e681ccdfd6efafc3ab2e
Jakob Pupke <jakob.pupke@gmail.com>
2018-04-18 22:57:27 +0200

    allow country option to be a string or symbol or proc

--- lib/active_model/telephone_number_validator.rb
+++ lib/active_model/telephone_number_validator.rb
@@ -1,9 +1,14 @@
 class TelephoneNumberValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    country = options[:country].call(record) if options.key?(:country)
+    country_option = options[:country]
+    country = if country_option.is_a? Proc
+                country_option.call(record)
+              elsif country_option.is_a?(Symbol) || country_option.is_a?(String)
+                # make sure its a lowercase symbol
+                country_option.downcase.to_sym
+              end
     valid_types = options.fetch(:types, [])
     args = [value, country, valid_types]
-
     record.errors.add(attribute, message) if TelephoneNumber.invalid?(*args)
   end
 
