d42f21a3ac30bb87345c485e0d929303987dd3b3
Jakob Pupke <jakob.pupke@gmail.com>
2018-04-18 23:24:06 +0200

    added tests for country option as string and symbol

--- test/active_model/model_test.rb
+++ test/active_model/model_test.rb
@@ -16,24 +16,40 @@ class ModelTest < Minitest::Test
     validates :phone_number, telephone_number: { country: proc{ |record| record.country } }
   end
 
+  class CountryValidationWithSymbol < BasicModel
+    validates :phone_number, telephone_number: { country: :us }
+  end
+
+  class CountryValidationWithString < BasicModel
+    validates :phone_number, telephone_number: { country: 'US' }
+  end
+
   class ValidationWithTypes < BasicModel
     validates :phone_number, telephone_number: { country: proc{ |record| record.country }, types: [:toll_free] }
   end
+
 
   class AllowBlankOption < BasicModel
     validates :phone_number, telephone_number: { country: proc{ |record| record.country } }, allow_blank: true
   end
 
   def test_basic_correctly_validates
-    assert BasicValidation.new(phone_number: "3175082489").valid?
+    basic_classes = [BasicValidation, CountryValidationWithSymbol, CountryValidationWithString]
+    basic_classes.each do |klass|
+      assert klass.new(phone_number: '3175082489').valid?
+    end
 
-    validation1 = BasicValidation.new(phone_number: "8")
-    assert validation1.invalid?
-    assert_includes validation1.errors, :phone_number
+    basic_classes.each do |klass|
+      validation1 = klass.new(phone_number: '8')
+      assert validation1.invalid?
+      assert_includes validation1.errors, :phone_number
+    end
 
-    validation2 = BasicValidation.new(phone_number: nil)
-    assert validation2.invalid?
-    assert_includes validation2.errors, :phone_number
+    basic_classes.each do |klass|
+      validation2 = klass.new(phone_number: nil)
+      assert validation2.invalid?
+      assert_includes validation2.errors, :phone_number
+    end
   end
 
   def test_with_types_correctly_validates
