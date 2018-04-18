509f98e1f45d7f14aa37c2bc3974f5c6c48e1479
Jakob Pupke <jakob.pupke@gmail.com>
2018-04-18 23:00:54 +0200

    country to method

--- lib/active_model/telephone_number_validator.rb
+++ lib/active_model/telephone_number_validator.rb
@@ -1,18 +1,21 @@
 class TelephoneNumberValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    country_option = options[:country]
-    country = if country_option.is_a? Proc
-                country_option.call(record)
-              elsif country_option.is_a?(Symbol) || country_option.is_a?(String)
-                # make sure its a lowercase symbol
-                country_option.downcase.to_sym
-              end
     valid_types = options.fetch(:types, [])
-    args = [value, country, valid_types]
+    args = [value, country(record), valid_types]
     record.errors.add(attribute, message) if TelephoneNumber.invalid?(*args)
   end
 
   def message
     options[:message] || :invalid
+  end
+
+  def country(record)
+    country_option = options[:country]
+    if country_option.is_a? Proc
+      country_option.call(record)
+    elsif country_option.is_a?(Symbol) || country_option.is_a?(String)
+      # make sure its a lowercase symbol
+      country_option.downcase.to_sym
+    end
   end
 end
