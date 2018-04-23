2e92b9e34f4ae513c7894f59e0889e951927081b
Jakob Pupke <jakob.pupke@googlemail.com>
2018-04-23 17:52:43 +0200

    Validator: Configurable error message + more flexible country option (#54)
    * add possibility to pass error message as option
    
    * allow country option to be a string or symbol or proc
    
    * country to method
    
    * readme + raise exception for wrong country arguments
    
    * added tests for country option as string and symbol
    
    * test for invalid country option
    
    * fixed test, invalid constr argument
    
    * test for active model error message

--- test/active_model/model_test.rb
+++ test/active_model/model_test.rb
@@ -12,8 +12,22 @@ class ModelTest < Minitest::Test
     end
   end
 
+  ERR_MESSAGE = 'This is not a valid telephone number!'
+
   class BasicValidation < BasicModel
-    validates :phone_number, telephone_number: { country: proc{ |record| record.country } }
+    validates :phone_number, telephone_number: { country: proc{ |record| record.country }, message: ERR_MESSAGE }
+  end
+
+  class CountryValidationWithSymbol < BasicModel
+    validates :phone_number, telephone_number: { country: :us, message: ERR_MESSAGE }
+  end
+
+  class CountryValidationWithString < BasicModel
+    validates :phone_number, telephone_number: { country: 'US', message: ERR_MESSAGE }
+  end
+
+  class CountryValidationWithArray < BasicModel
+    validates :phone_number, telephone_number: { country: [:us] }
   end
 
   class ValidationWithTypes < BasicModel
@@ -25,15 +39,29 @@ class ModelTest < Minitest::Test
   end
 
   def test_basic_correctly_validates
-    assert BasicValidation.new(phone_number: "3175082489").valid?
+    basic_classes = [BasicValidation, CountryValidationWithSymbol, CountryValidationWithString]
+    basic_classes.each do |klass|
+      assert klass.new(phone_number: '3175082489').valid?
+    end
 
-    validation1 = BasicValidation.new(phone_number: "8")
-    assert validation1.invalid?
-    assert_includes validation1.errors, :phone_number
+    basic_classes.each do |klass|
+      validation = klass.new(phone_number: '8')
+      assert validation.invalid?
+      assert_includes validation.errors, :phone_number
+    end
 
-    validation2 = BasicValidation.new(phone_number: nil)
-    assert validation2.invalid?
-    assert_includes validation2.errors, :phone_number
+    basic_classes.each do |klass|
+      validation = klass.new(phone_number: nil)
+      assert validation.invalid?
+      assert_includes validation.errors, :phone_number
+      assert_equal validation.errors[:phone_number].first, ERR_MESSAGE
+    end
+  end
+
+  def test_with_invalid_option_for_country
+    assert_raises ArgumentError do
+      CountryValidationWithArray.new(phone_number: '424324543645').valid?
+    end
   end
 
   def test_with_types_correctly_validates
