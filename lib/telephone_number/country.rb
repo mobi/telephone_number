23422efb466be1d6c23c2d9b4fed4f7cfc116ec7
Aaron Tavistock <aaron@geekshow.com>
2020-11-30 07:18:59 -0800

    Refactor out Hash extension (#90)

--- lib/telephone_number/country.rb
+++ lib/telephone_number/country.rb
@@ -7,18 +7,18 @@ module TelephoneNumber
     MOBILE_TOKEN_COUNTRIES = { AR: '9' }.freeze
 
     def initialize(data_hash)
+      validations = data_hash.fetch(:validations, {}).dup
       @country_code = data_hash[:country_code]
       @country_id = data_hash[:id]
-      @general_validation = NumberValidation.new(:general_desc, data_hash[:validations][:general_desc]) if data_hash.fetch(:validations, {})[:general_desc]
+      @general_validation = NumberValidation.new(:general_desc, validations[:general_desc]) if validations[:general_desc]
       @international_prefix = Regexp.new(data_hash[:international_prefix]) if data_hash[:international_prefix]
       @main_country_for_code = data_hash[:main_country_for_code] == 'true'
       @mobile_token = MOBILE_TOKEN_COUNTRIES[@country_id.to_sym]
       @national_prefix = data_hash[:national_prefix]
       @national_prefix_for_parsing = Regexp.new(data_hash[:national_prefix_for_parsing]) if data_hash[:national_prefix_for_parsing]
       @national_prefix_transform_rule = data_hash[:national_prefix_transform_rule]
-      @validations = data_hash.fetch(:validations, {})
-                              .except(:general_desc, :area_code_optional)
-                              .map { |name, data| NumberValidation.new(name, data) }
+      @validations = validations.delete_if { |name, _| name == :general_desc || name == :area_code_optional }
+                                .map { |name, data| NumberValidation.new(name, data) }
       @formats = data_hash.fetch(:formats, []).map { |format| NumberFormat.new(format, data_hash[:national_prefix_formatting_rule]) }
     end
 
@@ -52,11 +52,24 @@ module TelephoneNumber
     end
 
     def self.load_data
-      data_file = "#{File.dirname(__FILE__)}/../../data/telephone_number_data_file.dat"
-      main_data = Marshal.load(File.binread(data_file))
-      override_data = {}
-      override_data = Marshal.load(File.binread(TelephoneNumber.override_file)) if TelephoneNumber.override_file
-      return main_data.deep_deep_merge!(override_data)
+      data_path = "#{File.dirname(__FILE__)}/../../data/telephone_number_data_file.dat"
+      main_data = Marshal.load(File.binread(data_path))
+      if TelephoneNumber.override_file
+        override_data = Marshal.load(File.binread(TelephoneNumber.override_file))
+        main_data = deep_merge(main_data, override_data)
+      end
+      main_data
+    end
+
+    def self.deep_merge(base_hash, other_hash)
+      other_hash.each do |key, value|
+        base_hash[key] = if base_hash[key].is_a?(Hash) && value.is_a?(Hash)
+          deep_merge(base_hash[key], value)
+        else
+          value
+        end
+      end
+      base_hash
     end
 
     def self.all_countries
